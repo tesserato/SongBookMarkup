a774dd9b5cb88b37b6292ea22f189b65462b72fd
tesserato <tesserato@hotmail.com>
2021-09-28 17:19:23 -0300

    hosted as static site in azure

--- BuildAll.ps1
+++ BuildAll.ps1
@@ -1,7 +1,13 @@
 # flutter build windows
 # flutter pub run msix:create
 
-flutter build web --release
-firebase deploy
+# flutter build web --release
+# firebase deploy
+
+# az storage blob service-properties update --static-website --index-document "index.html" --account-name "markbookapp"
+
+az storage blob upload-batch -s ".\build\web\" -d '$web' --account-name "markbookapp" --content-type 'text/html; charset=utf-8'
+
+az storage account show -n "markbookapp" -g "MarkBook" --query "primaryEndpoints.web" --output tsv
 
 # flutter build apk
--- README.md
+++ README.md
@@ -3,3 +3,12 @@
 Yet another songbook app (thats my third attempt). I've been pondering this concept for a long time now, and the idea now is to consolidate an oppinionated app, with a minimalistic design and a concise set of features that just works, out of the box.
 
 the field of app development went a long way since my last attempt, and now i chose to realise it in flutter
+
+
+# links
+
+## App
+
+https://markbookapp.z13.web.core.windows.net/
+https://markbookapp-secondary.z13.web.core.windows.net/
+https://markbook-app.web.app/#/
