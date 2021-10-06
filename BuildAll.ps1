2386527921b9a9438270a344af326155513b7d9f
tesserato <tesserato@hotmail.com>
2021-10-06 20:40:44 -0300

    installer via Inno Setup

--- BuildAll.ps1
+++ BuildAll.ps1
@@ -2,15 +2,11 @@
 # flutter pub run flutter_launcher_icons:main
 
 
-# $cert = New-SelfSignedCertificate -certstorelocation cert:\localmachine\my -dnsname tesserato
-# $pwd = ConvertTo-SecureString -String ‘password1234’ -Force -AsPlainText
-# $path = ‘cert:\localMachine\my\’ + $cert.thumbprint
-# Export-PfxCertificate -cert $path -FilePath cert.pfx -Password $pwd
-# exit
+
+
 
 ## WINDOWS
 # flutter build windows
-flutter pub run msix:create
 exit
 
 flutter build web --release
@@ -22,3 +18,18 @@ az storage blob upload-batch -s ".\build\web\" -d '$web' --account-name "markboo
 az storage account show -n "markbookapp" -g "MarkBook" --query "primaryEndpoints.web" --output tsv
 
 # flutter build apk --release
+
+
+
+
+
+
+######################################################
+######################## msix ########################
+# $cert = New-SelfSignedCertificate -Type Custom -Subject "CN=Tesserato Software, O=Tesserato Corporation, C=BR" -KeyUsage DigitalSignature -FriendlyName "Mark Book installer certificate" -CertStoreLocation "Cert:\CurrentUser\My" -TextExtension @("2.5.29.37={text}1.3.6.1.5.5.7.3.3", "2.5.29.19={text}")
+
+# $password = ConvertTo-SecureString -String "1984" -Force -AsPlainText 
+# Export-PfxCertificate -cert ("Cert:\CurrentUser\My\" + $cert.thumbprint) -FilePath "cert.pfx" -Password $password
+# flutter pub run msix:create
+######################################################
+######################################################
